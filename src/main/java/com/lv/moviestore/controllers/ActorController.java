8cc825c49a9b8201d411e04580c55ba4a56581eb
TabKat <picated@protonmail.com>
2024-07-04 23:45:10 +0300

    - validation for Actor entity - @Validated to ActorController

--- src/main/java/com/lv/moviestore/controllers/ActorController.java
+++ src/main/java/com/lv/moviestore/controllers/ActorController.java
@@ -4,6 +4,7 @@ import com.lv.moviestore.models.Actor;
 import com.lv.moviestore.services.ActorService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @Slf4j
+@Validated
 @RestController
 @RequestMapping("/api/v1/actors")
 public class ActorController {
--- src/main/java/com/lv/moviestore/models/Actor.java
+++ src/main/java/com/lv/moviestore/models/Actor.java
@@ -1,6 +1,8 @@
 package com.lv.moviestore.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.sql.Timestamp;
@@ -17,11 +19,15 @@ public class Actor {
     Integer actorId;
 
     @Column(name = "first_name", nullable = false)
+    @NotBlank(message = "first name must not be blank")
+    @Size(min = 3, max = 20, message = "first name must be more that 2 symbols and not more then 20")
     String firstName;
 
     @Column(name = "last_name", nullable = false)
+    @NotBlank(message = "last name must not be blank")
+    @Size(min = 3, max = 20, message = "last name must be more that 2 symbols and not more then 20")
     String lastName;
 
-    @Column(name = "last_update")
+    @Column(name = "last_update", nullable = false)
     Timestamp lastUpdate;
 }
