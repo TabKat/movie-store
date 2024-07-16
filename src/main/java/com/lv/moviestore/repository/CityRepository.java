3581020cfadf39eb835ae495fd719faba6e45fcf
TabKat <picated@protonmail.com>
2024-07-16 23:07:26 +0300

    - add @Repository to CityRepository

--- src/main/java/com/lv/moviestore/repository/CityRepository.java
+++ src/main/java/com/lv/moviestore/repository/CityRepository.java
@@ -2,6 +2,8 @@ package com.lv.moviestore.repository;
 
 import com.lv.moviestore.models.City;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CityRepository extends JpaRepository<City, Long> {
 }
